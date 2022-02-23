c017760f7c32c81290a610cc57c8ec83d8314e72
Marcus Sommersel <marcus_sommersel@hotmail.com>
2022-02-23 12:15:57 +0100

    Added marching cubes

--- plotter.py
+++ plotter.py
@@ -1,6 +1,43 @@
 from matplotlib import projections
 import matplotlib.pyplot as plt
+import numpy as np
+from skimage import measure
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
+def readFile(filename):
+    f = open(filename)
+    
+    firstLine = f.readlines()[0].split(',')
+    f.close()
+    m = int(firstLine[0])
+    n = int(firstLine[1])
+    o = int(firstLine[2])
+
+    f = open(filename)
+    phi = np.zeros((m,n,o))
+    lines = f.readlines()[1:]
+    count = 0
+    for k in range(o):
+        for j in range(n):
+            for i in range(m):
+                phi[i,j,k] = float(lines[count].split(',')[3])
+                count += 1
+    return phi
+
+def getSurface(volume, level=0, plot=True):
+    verts, faces, normals, values = measure.marching_cubes(volume, level)
+    if plot:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.set_box_aspect([1,1,1])
+        mesh = Poly3DCollection(verts[faces])
+        mesh.set_edgecolor('k')
+        ax.add_collection3d(mesh)
+        plt.tight_layout()
+        ax.set_xlim(0, 9)
+        ax.set_ylim(0, 9)
+        ax.set_zlim(0, 9)
+        plt.show()
 
 def printScatter(filename):
     f = open(filename)
@@ -29,7 +66,9 @@ def printScatter(filename):
     plt.show()
 
 def main():
-    printScatter('file.txt')
+    phi = readFile('scaleField.txt')
+    getSurface(phi, 0, True)
+    # printScatter('file.txt')
 
 if __name__=='__main__':
     main()
