7f4693a8b07165ef2be55980c715821addbacc2e
Marcus Sommersel <marcus_sommersel@hotmail.com>
2022-03-08 15:37:13 +0100

    added functions and fixed bugs

--- vectorUtilities.cpp
+++ vectorUtilities.cpp
@@ -3,7 +3,7 @@
 vector<double> operator*(vector<double> const &vec1, vector<double> const &vec2){
     vector<double> res;
     if (vec1.size() != vec2.size()){
-        cerr << "Vectors multiplication with different sized vectors.";
+        cerr << "Vectors multiplication with different sized vectors." << endl;
     }
     for (int i = 0; i < vec1.size(); ++i){
         res.push_back(vec1[i]*vec2[i]);
@@ -11,10 +11,21 @@ vector<double> operator*(vector<double> const &vec1, vector<double> const &vec2)
     return res;
 }
 
+vector<double> operator/(vector<double> const &vec1, vector<double> const &vec2){
+    vector<double> res;
+    if (vec1.size() != vec2.size()){
+        cerr << "Vectors division with different sized vectors." << endl;
+    }
+    for (int i = 0; i < vec1.size(); ++i){
+        res.push_back(vec1[i]/vec2[i]);
+    }
+    return res;
+}
+
 vector<double> operator+(vector<double> const &vec1, vector<double> const &vec2){
     vector<double> res;
     if (vec1.size() != vec2.size()){
-        cerr << "Vectors addition with different sized vectors.";
+        cerr << "Vectors addition with different sized vectors." << endl;
     }
     for (int i = 0; i < vec1.size(); ++i){
         res.push_back(vec1[i] + vec2[i]);
@@ -33,7 +44,23 @@ vector<double> operator*(double const &scalar, vector<double> const &vec){
 vector<double> operator*(vector<double> const &vec, double const &scalar){
     vector<double> res;
     for (int i = 0; i < vec.size(); ++i){
-        res.push_back(scalar*vec[i]);
+        res.push_back(vec[i]*scalar);
+    }
+    return res;
+}
+
+vector<double> operator/(vector<double> const &vec, double const &scalar){
+    vector<double> res;
+    for (int i = 0; i < vec.size(); ++i){
+        res.push_back(vec[i]/scalar);
+    }
+    return res;
+}
+
+vector<double> operator/(double const &scalar, vector<double> const &vec){
+    vector<double> res;
+    for (int i = 0; i < vec.size(); ++i){
+        res.push_back(scalar/vec[i]);
     }
     return res;
 }
@@ -41,10 +68,87 @@ vector<double> operator*(vector<double> const &vec, double const &scalar){
 vector<double> operator-(vector<double> const &vec1, vector<double> const &vec2){
     vector<double> res;
     if (vec1.size() != vec2.size()){
-        cerr << "Vectors subtraction with different sized vectors.";
+        cerr << "Vectors subtraction with different sized vectors." << endl;
     }
     for (int i = 0; i < vec1.size(); ++i){
         res.push_back(vec1[i] - vec2[i]);
     }
     return res;
 }
+
+vector<double> operator+(vector<double> const &vec, double const &scalar){
+    vector<double> res;
+    for (int i = 0; i < vec.size(); ++i){
+        res.push_back(vec[i]+scalar);
+    }
+    return res;
+}
+
+vector<double> operator+(double const &scalar, vector<double> const &vec){
+    vector<double> res;
+    for (int i = 0; i < vec.size(); ++i){
+        res.push_back(scalar + vec[i]);
+    }
+    return res;
+}
+
+vector<double> operator-(vector<double> const &vec, double const &scalar){
+    vector<double> res;
+    for (int i = 0; i < vec.size(); ++i){
+        res.push_back(vec[i]-scalar);
+    }
+    return res;
+}
+
+vector<double> operator-(double const &scalar, vector<double> const &vec){
+    vector<double> res;
+    for (int i = 0; i < vec.size(); ++i){
+        res.push_back(scalar - vec[i]);
+    }
+    return res;
+}
+
+vector<double> vectorCBRT(vector<double> const &vec){
+    vector<double> res;
+    for (int i = 0; i < vec.size(); ++i){
+        res.push_back(cbrt(vec[i]));
+    }
+    return res;
+}
+
+vector<double> vectorAbs(vector<double> const &vec){
+    vector<double> res;
+    for (unsigned int i = 0; i < vec.size(); ++i){
+        if (vec[i] < 0){
+            res.push_back(-vec[i]);
+        } else {
+            res.push_back(vec[i]);
+        }
+    }
+    return res;
+}
+
+double vectorMax(vector<double> const &vec){
+    double max = numeric_limits<double>::lowest();
+    for (unsigned int i = 0; i < vec.size(); ++i){
+        if (isfinite(vec[i]) && (vec[i] > max)){
+            max = vec[i];
+        }
+    }
+    return max;
+}
+
+vector<double> vectorSqrt(vector<double> const &vec){
+    vector<double> res;
+    for (unsigned int i = 0; i < vec.size(); ++i){
+        res.push_back(sqrt(vec[i]));
+    }
+    return res;
+}
+
+// vector<double> vectorSign(vector<double> const &vec){
+//     vector<double> s;
+//     for (unsigned int i = 0; i < vec.size(); ++i){
+//         if (vec)
+//     }
+// }
--- vectorUtilities.h
+++ vectorUtilities.h
@@ -2,9 +2,13 @@
 
 #include <vector>
 #include <iostream>
+#include <cmath>
+#include <limits>
 using namespace std;
 
 vector<double> operator*(vector<double> const &vec1, vector<double> const &vec2);
+
+vector<double> operator/(vector<double> const &vec1, vector<double> const &vec2);
 
 vector<double> operator+(vector<double> const &vec1, vector<double> const &vec2);
 
@@ -12,4 +16,26 @@ vector<double> operator*(double const &scalar, vector<double> const &vec);
 
 vector<double> operator*(vector<double> const &vec, double const &scalar);
 
+vector<double> operator/(vector<double> const &vec, double const &scalar);
+
+vector<double> operator/(double const &scalar, vector<double> const &vec);
+
 vector<double> operator-(vector<double> const &vec1, vector<double> const &vec2);
+
+vector<double> operator+(vector<double> const &vec, double const &scalar);
+
+vector<double> operator+(double const &scalar, vector<double> const &vec);
+
+vector<double> operator-(vector<double> const &vec, double const &scalar);
+
+vector<double> operator-(double const &scalar, vector<double> const &vec);
+
+vector<double> vectorCBRT(vector<double> const &vec);
+
+vector<double> vectorAbs(vector<double> const &vec);
+
+double vectorMax(vector<double> const &vec);
+
+vector<double> vectorSqrt(vector<double> const &vec);
+
+// vector<double> vectorSign(vector<double> const &vec);
