7aec2447eae2f547e0bb9a0cdd1d0e8b2d915124
Marcus Sommersel <marcus_sommersel@hotmail.com>
2022-05-30 14:31:55 +0200

    final push

--- main.cpp
+++ main.cpp
@@ -12,14 +12,17 @@ using namespace std;
 
 int main(){ 
 
+    // start of setup.
     chrono::steady_clock::time_point startTime = chrono::steady_clock::now();
 
     cout << "Program start." << endl;
 
-    const int m = 50;
-    const int n = 50; 
-    const int p = 50;
+    // grid nodes in each spatial direction
+    const int m = 200; // x-direction
+    const int n = 200; // y-direction
+    const int p = 200; // z-direction
 
+    // size of domain
     const double xStart = 0.0;
     const double xEnd = 1.0;
     const double yStart = 0.0;
@@ -27,26 +30,31 @@ int main(){
     const double zStart = 0.0;
     const double zEnd = 1.0;
 
+    // vector of nodes in each direction
     vector<double> x = linspace(xStart, xEnd, m);
     vector<double> y = linspace(yStart, yEnd, n);
     vector<double> z = linspace(zStart, zEnd, p);
 
+    // grid spacing in each direction
     double dx = x[1] - x[0];
     double dy = y[1] - y[0];
     double dz = z[1] - z[0];
 
-    double dtau = 0.5*dx;
-    int reinitSteps = 5;
-    bool doReinit = true;
-    bool doParticle = true;
-    bool saveParticles = false;
-    int nParticles = 64;
-    int reinitFreq = 1;
-    int plotFreq = 100;
-    int reseedFreq = 100;
-    int itmax = 2000;
-    double CFL = 0.5;
-    bool halfplot = true;
+    // varius parameters that may be changed
+    double dtau = 0.5*dx; // size of pseudo-time step
+    bool doReinit = true; // true if reinitialization should be done
+    bool doParticle = false; // true if particles should be used for level set method
+    bool saveParticles = false; // true if particles should be saved for plotting
+    int nParticles = 64; // number of particles of each type (positive and negative) in each cell
+    int reinitFreq = 1; // how often reinitialization should be conducted
+    int reinitSteps = 5; // how many pseudo-time steps should be done
+    int plotFreq = 20000; // how often signed distance field is saved, not including first and last time step
+    int reseedFreq = 100; // how often particles are reseeded
+    int itmax = 20000; // maximum number of iterations
+    double CFL = 0.9; // CFL-number
+    bool halfplot = true; // true if signed distance field should be saved at 0.5 t/T
+
+    // The three implemented test cases
     string testcase = "vortex";
     // string testcase = "sheared";
     // string testcase = "simple";
@@ -59,11 +67,10 @@ int main(){
         c = Point(0.35,0.35,0.35);
         r = 0.15;
         T = 3.0;
-
     } else if (testcase == "sheared"){
         c = Point(0.5,0.75,0.5);
         r = 0.15;
-        T = 2.0;
+        T = 10.0;
     } else if (testcase == "simple"){
         c = Point(0.35,0.35,0.35);
         r = 0.15;
@@ -72,8 +79,10 @@ int main(){
 
     vector<double> phi;
 
+    // initial signed distance field
     signedDistanceField(phi, x, y, z, r, c, m, n, p);
 
+    // measures of error
     double initialVolume = volume(phi, dx, dy, dz); 
     vector<double> phi0 = phi;
     double MError0 = massError(phi, dx, dy, dz, m, n, p);
@@ -82,17 +91,18 @@ int main(){
     vector<string> plotTimes;
     vector<string> plotTimesParticle;
     
+    // save signed distance field
     saveScalarField(savePath + to_string(0.000000) + ".txt", phi, x, y, z, m, n, p);
-    plotTimes.push_back(savePath + to_string(0.000000));
+    plotTimes.push_back(to_string(0.000000));
 
-
+    // parameters used in particle level set method
     vector<Particle> particles;
     double rmin = 0.1*min(dx, min(dy, dz));
     double rmax = 0.5*max(dx, max(dy, dz));
     double bmin = rmin;
     double bmax = 3.0*max(dx, max(dy, dz));
 
-    // Initializing particles
+    // initializing particles
     cout << "Initializing particles." << endl;
     if (doParticle){
         Derivative norm = normal(phi, dx, dy, dz, m, n, p);
@@ -111,52 +121,58 @@ int main(){
 
         if (saveParticles){
             plotParticles(savePath + to_string(0.000000) + "particle.txt" , particles);
-            plotTimesParticle.push_back(savePath + to_string(0.000000) + "particle");
+            plotTimesParticle.push_back(to_string(0.000000) + "particle");
         }
     }
 
-    double dt = 0;
+    vector<double> ax;
+    vector<double> ay;
+    vector<double> az;
+
     double t = 0;
+
+    // initial velocity field
+    if (testcase == "vortex"){
+        Velocity a = vortexVelocity(m, n, p, x, y, z, t, T);
+        ax = a.x;
+        ay = a.y;
+        az = a.z;
+    } else if (testcase == "sheared"){
+        Velocity a = shearedSphereVelocity(m, n, p, x, y, z, t, T);
+        ax = a.x;
+        ay = a.y;
+        az = a.z;
+    } else if (testcase == "simple"){
+        Velocity a = simpleVelocity(m, n, p);
+        ax = a.x;
+        ay = a.y;
+        az = a.z;
+    }
+
+    double dtmax = CFL/(vectorMax(vectorAbs(ax)/dx + vectorAbs(ay)/dy + vectorAbs(az)/dz)); // max time step
+    double dt;
     int numIt = 0;
 
     cout << "Setup complete." << endl;
     chrono::steady_clock::time_point currentTime = chrono::steady_clock::now();
     cout << "Elapsed time: " << (chrono::duration_cast<chrono::seconds>(currentTime - startTime).count())  << " s." << endl << endl;
 
-    vector<double> ax;
-    vector<double> ay;
-    vector<double> az;
-
+    // main iteration loop
     for (int it = 1; it < itmax; ++it){
 
-        if (testcase == "vortex"){
-            Velocity a = vortexVelocity(m, n, p, x, y, z, t, T);
-            ax = a.x;
-            ay = a.y;
-            az = a.z;
-        } else if (testcase == "sheared"){
-            Velocity a = shearedSphereVelocity(m, n, p, x, y, z, t, T);
-            ax = a.x;
-            ay = a.y;
-            az = a.z;
-        } else if (testcase == "simple"){
-           Velocity a = simpleVelocity(m, n, p);
-            ax = a.x;
-            ay = a.y;
-            az = a.z;
-        }
-        
-        dt = CFL/(vectorMax(vectorAbs(ax)/dx + vectorAbs(ay)/dy + vectorAbs(az)/dz));
-
-        if (t + dt > T){
-            dt = T - t;
+        // statement to make sure loop has one iteration at 0.5 t/T
+        if (t < 0.5*T){
+            dt = min(dtmax, 0.5*T - t);
+        } else if (t >= 0.5*T){
+            dt = min(dtmax, T - t);
         }
 
         t += dt;
 
+        // advection of level set method
         TVDRK3_weno(phi, ax, ay, az, m, n, p, dx, dy, dz, dt);
 
-        // Advect particles
+        // advection of particles
         if (doParticle){
             for (int a = 0; a < particles.size(); ++a){
                 
@@ -216,7 +232,7 @@ int main(){
                     phi[(i+1)+(j+1)*n+(k+1)*p*p], 
                     phi[i+(j+1)*n+(k+1)*p*p]);
 
-                // Delete particles
+                // delete particles
                 if (abs(phip) - particles[a].r > bmax){
                     particles.erase(particles.begin() + a);
                 }
@@ -257,7 +273,7 @@ int main(){
                     phi[(i+1)+(j+1)*n+(k+1)*p*p], 
                     phi[i+(j+1)*n+(k+1)*p*p]);
 
-                // Adjust radius
+                // adjust radius
                 if (sign(phip)*phip > rmax){
                     particles[a].r = rmax;
                 } else if (sign(phip)*phip < rmin){
@@ -268,7 +284,7 @@ int main(){
 
             }
 
-            // Initialize new particles
+            // initialize new particles
             if (it%reseedFreq == 0 && it != 0){
                 vector<int> cellx;
                 vector<int> celly;
@@ -312,6 +328,7 @@ int main(){
             }
         }
 
+        // reinitialization loop
         if (doReinit && (it%reinitFreq == 0)){
             vector<double> phi0 = phi;
             for (int i = 0; i < reinitSteps - 1; ++i){
@@ -320,8 +337,10 @@ int main(){
             }
         }
 
+        // mass error
         MError += abs(massError(phi, dx, dy, dz, m, n, p) - MError0)*dt;
 
+        // printing to console
         if (it%10 == 0){
             cout << "Iteration: " << it << endl;
             chrono::steady_clock::time_point currentTime = chrono::steady_clock::now();
@@ -331,28 +350,50 @@ int main(){
         if (it%plotFreq == 0){
             cout << "Saving scalar field." << endl;
             saveScalarField(savePath + to_string(t) + ".txt", phi, x, y, z, m, n, p);
-            plotTimes.push_back(savePath + to_string(t));
+            plotTimes.push_back(to_string(t));
 
             if (doParticle && saveParticles){
                 plotParticles(savePath + to_string(t) + "particle.txt" , particles);
-                plotTimesParticle.push_back(savePath + to_string(t) + "particle");
+                plotTimesParticle.push_back(to_string(t) + "particle");
             }
             cout << "Done saving." << endl;
         }
 
-        if (t/T > 0.5 && halfplot){
+        // plotting at 0.5 t/T
+        if (t/T == 0.5 && halfplot){
+            cout << "Saving scalar field." << endl;
             saveScalarField(savePath + to_string(t) + ".txt", phi, x, y, z, m, n, p);
-            plotTimes.push_back(savePath + to_string(t));
-            halfplot = false;
+            plotTimes.push_back(to_string(t));
+            cout << "Done saving." << endl;
         }
 
+        // finish iterations if t = T
         if (t == T){
             numIt = it;
             break;
         }
 
+        // find velocity for next time step
+        if (testcase == "vortex"){
+            Velocity a = vortexVelocity(m, n, p, x, y, z, t, T);
+            ax = a.x;
+            ay = a.y;
+            az = a.z;
+        } else if (testcase == "sheared"){
+            Velocity a = shearedSphereVelocity(m, n, p, x, y, z, t, T);
+            ax = a.x;
+            ay = a.y;
+            az = a.z;
+        } else if (testcase == "simple"){
+            Velocity a = simpleVelocity(m, n, p);
+            ax = a.x;
+            ay = a.y;
+            az = a.z;
+        }
+
     }
 
+    // writing last time step to console
     {
         cout << "Iteration: " << numIt << endl;
         chrono::steady_clock::time_point currentTime = chrono::steady_clock::now();
@@ -360,17 +401,19 @@ int main(){
         cout << "t = " << t << endl;
     }
 
+    // error measures
     double endVolume = volume(phi, dx, dy, dz);
     double volumeChange = 100*(endVolume-initialVolume)/initialVolume;
     double L1Error = interfaceError(phi0, phi, dx, dy, dz, m, n, p);
     MError = MError/t;
 
+    // saving of signed distance field for final time step
     saveScalarField(savePath + to_string(T) + ".txt", phi, x, y, z, m, n, p);
-    plotTimes.push_back(savePath + to_string(T));
+    plotTimes.push_back(to_string(T));
 
     if (doParticle && saveParticles){
         plotParticles(savePath + to_string(t) + "particle.txt" , particles);
-        plotTimesParticle.push_back(savePath + to_string(t) + "particle");
+        plotTimesParticle.push_back(to_string(t) + "particle");
     }
 
     {
@@ -393,6 +436,7 @@ int main(){
         file.close();
     }
 
+    // printing log to file
     {
         ofstream file;
         file.open(savePath + "log.txt");
